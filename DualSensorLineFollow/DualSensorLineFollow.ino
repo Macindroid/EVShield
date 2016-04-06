5944bdf9a5d1d3ef67c972aa2f10ea16fe49047a
Macindroid <acaughey@macindroid.com>
2016-04-06 16:14:00 +1000

    Line Follow Edit
    Dual sensor line follow algorithm done + DIY Button Wired in through program.

--- DualSensorLineFollow/DualSensorLineFollow.ino
+++ DualSensorLineFollow/DualSensorLineFollow.ino
@@ -5,9 +5,12 @@ EVShield     GEVShield(0x34,0x36);
 EVs_NXTLight GLS1;
 EVs_NXTLight GLS2;         
 
+const int CConfirmPin = 4;
 
 void setup() 
 {
+    pinMode(CConfirmPin, INPUT);
+    
   
     Serial.begin(9600);
     Serial.println ("-------------------------------------");
@@ -16,11 +19,12 @@ void setup()
     GEVShield.init( SH_HardwareI2C );
     GLS1.init(&GEVShield, SH_BBS1);
     GLS2.init(&GEVShield, SH_BBS2);
-    GLightSensor.setReflected();
+    GLS1.setReflected();
+    GLS2.setReflected();
     GEVShield.bank_b.motorReset();  
 
     Serial.println("Press Confirm To Continue");
-    while (GEVShield.getButtonState(BTN_GO) != true)
+    while (digitalRead(CConfirmPin) != 1)
     {
       delay(500);
     }
@@ -32,12 +36,41 @@ void loop()
   int LLightValue2;
   // Raw Light Value 
   
-  int LTreshhold1 = 643; 
-  int LTreshhold2 = 643; 
+  int LTreshold1 = 643; 
+  int LTreshold2 = 643; 
   // Replace with auto calibration / control board switch
   
   int LMotorPowerOutside;
   int LMotorPowerInside;
   // Motor Turn Ratio
 
+  LLightValue1 = GLS1.readRaw();
+  LLightValue2 = GLS2.readRaw();
+  Serial.println(LLightValue1);
+  Serial.println(LLightValue2);
+
+  if (LLightValue1 < LTreshold1 && LLightValue2 < LTreshold2)      // White & White
+  {
+    GEVShield.bank_b.motorRunUnlimited(SH_Motor_1, SH_Direction_Forward, LMotorPowerOutside);
+    GEVShield.bank_b.motorRunUnlimited(SH_Motor_2, SH_Direction_Forward, LMotorPowerOutside); 
+  }
+  else if (LLightValue1 < LTreshold1 && LLightValue2 > LTreshold2) // White & Black
+  {
+    GEVShield.bank_b.motorRunUnlimited(SH_Motor_1, SH_Direction_Reverse, LMotorPowerOutside);
+    GEVShield.bank_b.motorRunUnlimited(SH_Motor_2, SH_Direction_Forward, LMotorPowerInside); 
+  }
+  else if (LLightValue1 > LTreshold1 && LLightValue2 < LTreshold2) // Black & White
+  {
+    GEVShield.bank_b.motorRunUnlimited(SH_Motor_1, SH_Direction_Reverse, LMotorPowerOutside);
+    GEVShield.bank_b.motorRunUnlimited(SH_Motor_2, SH_Direction_Forward, LMotorPowerInside); 
+  }
+  else if (LLightValue1 > LTreshold1 && LLightValue2 > LTreshold2) // Black & Black (Green / Turn Hints)
+  {
+    Serial.println("Green");
+  }
+  else //Silver
+  {
+    Serial.println("Silver");
+  }
+  
 }
