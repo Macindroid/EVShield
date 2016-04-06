324b279847b3e3b0e489be03683c6514f5ee2743
Macindroid <acaughey@macindroid.com>
2016-04-06 14:37:12 +1000

    Added Files
    Added Control Board Test Files

---
+++ DIY Control Board/Control Board.ino
@@ -0,0 +1,36 @@
+
+const int CSwitchModePin = 2;
+const int CSwitchAcceptPin = 4;
+const int CLedPin1 =  7;
+const int CLedPin2 =  8;
+const int CLedPin3 =  12;
+
+void setup() {
+  pinMode(CLedPin1, OUTPUT);
+  pinMode(CLedPin2, OUTPUT);
+  pinMode(CLedPin3, OUTPUT);
+  pinMode(CSwitchModePin, INPUT);
+  pinMode(CSwitchAcceptPin, INPUT);
+
+  Serial.begin(9600);
+}
+
+void loop() {
+  digitalWrite(CLedPin1, HIGH);
+  digitalWrite(CLedPin2, LOW);
+  digitalWrite(CLedPin3, LOW);
+  delay(500);
+  digitalWrite(CLedPin1, LOW);
+  digitalWrite(CLedPin2, HIGH);
+  digitalWrite(CLedPin3, LOW);
+  delay(500);
+  digitalWrite(CLedPin1, LOW);
+  digitalWrite(CLedPin2, LOW);
+  digitalWrite(CLedPin3, HIGH);
+  delay(500);
+  Serial.println("");
+  Serial.print("Switch Mode: "); Serial.print(digitalRead(CSwitchModePin)); Serial.println("");
+  Serial.print("Switch Accept: "); Serial.print(digitalRead(CSwitchAcceptPin)); Serial.println("");
+
+  
+}
